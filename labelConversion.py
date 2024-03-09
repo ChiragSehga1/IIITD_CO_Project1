e1158458d4a69f43a65d4590be767d5c3dd1019d
Chirag Sehgal <chirag23176@iiitd.ac.in>
2024-03-10 00:49:40 +0530

    Added removeAllLabels to labelConversion.py
    All label names will be removed. Lines that contain only label names will become empty lines (instead of being removed) 
    in order to preserve line number order. Lines that have instructions after the label name will work as is, with the label name
    removed from the beginning.

--- labelConversion.py
+++ labelConversion.py
@@ -1,33 +1,42 @@
-"""The final hashmap will have '\n' character removed from end of lines, with all labels converted to a number as
+"""
+The last function is all that needs to run, which will make 2 major changes:
+
+1. The final hashmap will have '\n' character removed from end of lines, with all labels converted to a number as
 diff=labelLineNum-currentLineNum, that is, the line of the label relative to the current line
-Thus, to get to the label, we'll do currentLineNum + diff, reaching that label line"""
+Thus, to get to the label, we'll do currentLineNum + diff, reaching that label line
+
+2. All label names will be removed. Lines that contain only label names will become empty lines (instead of being removed) 
+in order to preserve line number order. Lines that have instructions after the label name will work as is, with the label name
+removed from the beginning.
+"""
 
 def readFile():
     """Reads code from file and store them in a numbered hashmap, as well as number of lines in program"""
-    file=open(r"C:\Users\user\OneDrive\Desktop\Chirag_CO\Assembly.txt",'r')
+    file=open(r"C:\Users\gaurm\OneDrive\Desktop\Assembly.txt",'r')
     code=list(file.readlines())
     lineNumber=1
     numberedCode={}
+    if code[-1][-1]!='n':
+        code[-1]=code[-1]+'\n'
     for line in code:
         numberedCode[lineNumber]=line[0:-1]
         lineNumber+=1
     return numberedCode,lineNumber-1
 
 def BType(a):
-    """checks if an instruction line 'a' is BType""""
+    """checks if an instruction line 'a' is BType"""
     l=["beq " , "bne " , "bge " , "bgeu" , "blt " , "bltu"]
     if a[0:4] in l:
         return True
     return False
 
 def JType(a):
-    """checks if an instruction line 'a' is JType""""
+    """checks if an instruction line 'a' is JType"""
     return a[0:3]=="jal"
 
 def labelConvert():
     """Converts all labels in the numberedCode to line difference between current and label line, and returns a numbered hashmap of the same"""
     numberedCode,numberOfLines=readFile()
-    labelConverted={}
     #making a dictionary while label converting
     for i in range(1,numberOfLines+1):
         start=numberedCode[i].find(':')+1
@@ -40,7 +49,13 @@ def labelConvert():
                     diff=k-currentLineNum
                     break
             index=numberedCode[i].find(label)
-            labelConverted[i]=numberedCode[i][0:index]+str(diff)
-        else:
-            labelConverted[i]=numberedCode[i]
+            numberedCode[i]=numberedCode[i][0:index]+str(diff)
+    return numberedCode
+
+def removeAllLabels():
+    labelConverted=labelConvert()
+    for k,v in labelConverted.items():
+        a=v.find(':')+1
+        print(k,v,a)
+        labelConverted[k]=v[a:]
     return labelConverted
