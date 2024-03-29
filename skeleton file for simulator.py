15e3a5ef1ccb9815ddb6085228a880d25bbad096
Dhruv Jaiswal <160203801+CobaltIII@users.noreply.github.com>
2024-03-29 23:45:30 +0530

    Update skeleton file for simulator.py

--- skeleton file for simulator.py
+++ skeleton file for simulator.py
@@ -27,6 +27,7 @@ lw a7, 53(a2)
 This is the sample I will write the requirments in
 """
 
+#AKSHAT
 # program stub to make a dictionary
 # returns a dictionary of the form {line number(in bytes)<int> : [line<str> , instruction type<char/str>]}
 # write "bonus" for the bonus instructions
@@ -36,12 +37,14 @@ This is the sample I will write the requirments in
 #                                                          12 : ["00000000000000000000000001100011" , "B"]]}
 
 
+#DHRUV
 # program stub to iterate through the entire code and find errors, raise errors if any
 # possible errors till now - undefined opcode
 #                            virtual halt not present/not at the end
 #                            ...(maybe more to be added)
 
 
+#JASJYOT
 # program stub to convert binary to decimal which takes 2 arguments, number of bits<int> 
 # and the number <str> and returns a decimal number<int>
 
@@ -58,13 +61,13 @@ This is the sample I will write the requirments in
 # line_to_execute = dictionary[program_counter][0]
 # type_of_intruction = dictionary[program_counter][1]
 
-# code to execute R type instruction
-# code to execute I type instruction
-# code to execute S type instruction
-# code to execute B type instruction (needs to break loop if "00000000000000000000000001100011" found)
-# code to execute U type instruction
-# code to execute J type instruction
-# code to execute bonus type instruction
+# code to execute R type instruction #AKSHAT
+# code to execute I type instruction #AKSHAT
+# code to execute S type instruction #CHIRAG
+# code to execute B type instruction (needs to break loop if "00000000000000000000000001100011" found) #CHIRAG
+# code to execute U type instruction #JASJYOT
+# code to execute J type instruction #JASJYOT
+# code to execute bonus type instruction #DHRUV
 
 ### ALL CODES ABOVE ARE ON SWITCH-CASE BASIS. THEY NEED TO EDIT THE PROGRAM COUNTER AND REGISTERS INTERNALLY.
 ### If virtual halt ("B" type) or halt ("bonus" type) are found, we need to change the global variable stop and exit loop
